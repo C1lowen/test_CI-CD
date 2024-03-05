8d7023a7b6365e8cd2f203c5b091141268229805
C1lowen <evgenijobedkov@gmail.com>
2024-03-05 19:56:17 +0200

    code

---
+++ src/main/java/com/example/testcicd/TestCicdApplication.java
@@ -0,0 +1,21 @@
+package com.example.testcicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class TestCicdApplication {
+
+    @GetMapping("/hello")
+    public String testHello() {
+        return "hello user!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestCicdApplication.class, args);
+    }
+
+}
